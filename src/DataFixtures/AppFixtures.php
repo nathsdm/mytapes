0f884a15027fa247ce290f17613aca134e607a31
nathsdm <nathan.soares_de_melo@telecom-sudparis.eu>
2023-10-04 13:25:14 +0200

    Added data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Inventory;
+use App\Entity\Tape;
+use App\Entity\Member;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,6 +12,22 @@ class AppFixtures extends Fixture
 {
     private const SEB_INVENTORY = 'seb-inventory';
     private const OLIVIER_INVENTORY = 'olivier-inventory';
+
+    /**
+     * Generates initialization data for members
+     * @return \\Generator
+     */
+    private static function memberDataGenerator()
+    {
+        yield [
+            'name' => 'Seb',
+            'reference' => self::SEB_INVENTORY,
+        ];
+        yield [
+            'name' => 'Olivier',
+            'reference' => self::OLIVIER_INVENTORY,
+        ];
+    }
 
     /**
      * Generates initialization data for inventories
@@ -20,10 +38,12 @@ class AppFixtures extends Fixture
         yield [
             'name' => 'Seb\'s inventory',
             'reference' => self::SEB_INVENTORY,
+            'member' => 'Seb',
         ];
         yield [
             'name' => 'Olivier\'s inventory',
             'reference' => self::OLIVIER_INVENTORY,
+            'member' => 'Olivier',
         ];
     }
 
@@ -51,16 +71,32 @@ class AppFixtures extends Fixture
             'year' => 1994,
             'inventory' => self::SEB_INVENTORY,
         ];
+        yield [
+            'name' => 'エコーチャンバーパーティー',
+            'artist' => 'Macroblank',
+            'year' => 2021,
+            'inventory' => self::OLIVIER_INVENTORY,
+        ];
     }
 
     public function load(ObjectManager $manager)
     {
         $inventoryrepo = $manager->getRepository(Inventory::class);
 
+        foreach (self::memberDataGenerator() as $memberData) {
+            $member = new Member();
+            $member->setName($memberData['name']);
+            $manager->persist($member);
+            $manager->flush();
+            $this->addReference($memberData['name'], $member);
+        }
+
         foreach (self::inventoryDataGenerator() as $inventoryData) {
             $inventory = new Inventory();
             $inventory->setName($inventoryData['name']);
+            $inventory->setMember($this->getReference($inventoryData['member']));
             $manager->persist($inventory);
+            $manager->flush();
             $this->addReference($inventoryData['reference'], $inventory);
         }
 
@@ -72,5 +108,6 @@ class AppFixtures extends Fixture
             $tape->setInventory($this->getReference($tapeData['inventory']));
             $manager->persist($tape);
         }
+        $manager->flush();
     }
 }
