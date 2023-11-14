bcf616c9b322c6acd69a1e511d219b01cb8add61
nathsdm <nathan.soares_de_melo@telecom-sudparis.eu>
2023-11-14 10:45:42 +0100

    Added back admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Entity\Inventory;
 use App\Entity\Tape;
-use App\Entity\Member;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
